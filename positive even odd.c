bb42530c5c9aa26ca6463ac9a0c6ff57d72b9a56
SushantChavan1 <114819811+SushantChavan1@users.noreply.github.com>
2022-10-01 20:29:44 +0530

    Add files via upload

---
+++ positive even odd.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+ int n;
+ printf("enter the value of n");
+ scanf("%d",&n);
+ if(n>0)
+ { 
+   if(n%2==0)
+   { 
+     printf(" n is positive even");
+    }
+    else
+     { 
+      printf(" n is positive odd");
+      }
+    }
+    else 
+      { 
+        if (n%2==0)
+         { 
+           printf("  n is negative even");
+          }
+        else 
+          {
+            printf(" n is negative odd");
+           }
+        }
+        getch();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+ 
+            
+            
+            
+            
+            
+            
+            
+            
+            
